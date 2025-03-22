be1498d189fad137505082b1731306ffb09c9ca5
wasabitatakituna <144505175+wasabitatakituna@users.noreply.github.com>
2025-03-22 16:37:30 -0700

    making the pdf look nice

--- frontend/src/components/Try.jsx
+++ frontend/src/components/Try.jsx
@@ -43,46 +43,48 @@ function Try() {
     }
 
     return (
-        <div className="popUp text-teal mb-25">
-            <div className="the-top bg-dusty-blue">
-                <ul>
-                    <li className="tabBut"/>
-                    <li className="tabBut"/>
-                    <li className="tabBut"/>
-                </ul>
-            </div>
-            <h2 className="mt-5 mb-10">Try It Now!!!</h2>
-
-            <form action="" onSubmit={HandleSubmit}>
-                <textarea
-                    type="text"
-                    id="txt"
-                    name="txt"
-                    placeholder="Enter your ideas here..."
-                    value={txt}
-                    onChange={(e) => setTxt(e.target.value)}
-                    className="text-light-cream p-2 mb-5">
-                </textarea>
-
-                <br />
-
-                <button
-                type="submit"
-                className="w-50 h-full bg-dusty-blue text-white rounded-lg px-4 py-2 mb-10"
-                >Submit Pitch</button>
-            </form>
-
-            {pdfUrl && (
-                <div className="w-full mx-auto h-screen bg-white overflow-hidden">
-                    <iframe
-                        src={`${pdfUrl}#view=FitH&pagemode=none&toolbar=0&navpanes=0`}
-                        title="PDF Presentation"
-                        className="w-full h-full"
-                        style={{border: 'none'}}
-                    ></iframe>
+        <div>
+            <div className="popUp text-teal mb-25">
+                <div className="the-top bg-dusty-blue">
+                    <ul>
+                        <li className="tabBut"/>
+                        <li className="tabBut"/>
+                        <li className="tabBut"/>
+                    </ul>
                 </div>
-            )}
-        </div>
+                <h2 className="mt-5 mb-10">Try It Now!!!</h2>
+
+                <form action="" onSubmit={HandleSubmit}>
+                    <textarea
+                        type="text"
+                        id="txt"
+                        name="txt"
+                        placeholder="Enter your ideas here..."
+                        value={txt}
+                        onChange={(e) => setTxt(e.target.value)}
+                        className="text-light-cream p-2 mb-5">
+                    </textarea>
+                    
+                    <br />
+
+                    <button
+                    type="submit"
+                    className="w-50 h-full bg-dusty-blue text-white rounded-lg px-4 py-2 mb-10"
+                    >Submit Pitch</button>
+                </form>
+            </div>
+
+                {pdfUrl && (
+                    <div className="myPDF w-full mx-auto h-screen bg-white overflow-hidden">
+                        <iframe
+                            src={`${pdfUrl}#view=FitH&pagemode=none&toolbar=0&navpanes=0`}
+                            title="PDF Presentation"
+                            className="w-full h-full"
+                            style={{border: 'none'}}
+                        ></iframe>
+                    </div>
+                )}
+            </div>
     );
 }
 
