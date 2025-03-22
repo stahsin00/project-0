d59a72c22bbb2c3fd1f99d252b1a3979c1599653
wasabitatakituna <144505175+wasabitatakituna@users.noreply.github.com>
2025-03-22 15:43:36 -0700

    working on results page

--- frontend/src/components/Try.jsx
+++ frontend/src/components/Try.jsx
@@ -3,6 +3,33 @@ import { React, useState } from "react";
 function Try() {
     const [txt, setTxt] = useState("");
 
+    async function HandleSubmit(e) {
+        e.preventDefault();
+
+        // "gen" for "generate response"
+        // const apiUrl = 'http://localhost:3000/api/generate';
+        // fetch(apiUrl)
+        // .then((res) => res.json())
+        // .then((gen) => {
+        //     setAppState({ loading: false, gen: gen.slice(0,4) });
+        // });
+
+        const response = await fetch(
+        'http://localhost:3000/api/generate',
+            {
+              method: 'POST',
+              body: JSON.stringify({ "topic": txt }),
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            }
+        );
+
+        setTxt("");
+        const result = await response.json();
+        console.log(result);
+    }
+
     return (
         <div className="popUp text-teal mb-25">
             <div className="the-top bg-dusty-blue">
@@ -14,15 +41,24 @@ function Try() {
             </div>
             <h2 className="mt-5 mb-10">Try It Now!!!</h2>
 
-            <textarea
-                type="text"
-                id="txt"
-                name="txt"
-                placeholder="Enter your ideas here..."
-                value={txt}
-                onChange={(e) => setTxt(e.target.value)}
-                className="text-light-cream p-2 mb-15">
-            </textarea>
+            <form action="" onSubmit={HandleSubmit}>
+                <textarea
+                    type="text"
+                    id="txt"
+                    name="txt"
+                    placeholder="Enter your ideas here..."
+                    value={txt}
+                    onChange={(e) => setTxt(e.target.value)}
+                    className="text-light-cream p-2 mb-5">
+                </textarea>
+
+                <br />
+
+                <button
+                type="submit"
+                className="w-50 h-full bg-dusty-blue text-white rounded-lg px-4 py-2 mb-10"
+                >Submit Pitch</button>
+            </form>
         </div>
     );
 }
